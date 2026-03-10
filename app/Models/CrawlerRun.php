4258dcc08ee4a9dd7846308feee07b18526660b7
jordi-torras-ai <143268981+jordi-torras-ai@users.noreply.github.com>
2026-03-10 09:45:34 +0100

    bug fix

--- app/Models/CrawlerRun.php
+++ app/Models/CrawlerRun.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\CarbonInterval;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\CarbonInterval;
 
 class CrawlerRun extends Model
 {
